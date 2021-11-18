76a996ca98f3177d3ad711094a7ba2e095066a25
Tomás Silva <tomas-250@outlook.com>
2021-11-18 03:20:13 +0000

    Inclusão de comentários de autoria

--- engine.h
+++ engine.h
@@ -1,3 +1,7 @@
+// ISEC - Trabalho Prático de POO 2021/2022
+// Tomás Gomes Silva - 2020143845
+// Tomás da Cunha Pinto - 2020144067
+
 #ifndef ENGINE
 #define ENGINE
 
--- main.cpp
+++ main.cpp
@@ -1,3 +1,7 @@
+// ISEC - Trabalho Prático de POO 2021/2022
+// Tomás Gomes Silva - 2020143845
+// Tomás da Cunha Pinto - 2020144067
+
 #include "engine.h"
 #include <iostream>
 #include <string>
